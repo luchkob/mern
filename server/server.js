57a77802c559a2276823622f076a37e5c81f7f42
luchkob <umy451710@gmail.com>
2022-09-20 16:34:02 +0700

    new version

--- server/server.js
+++ server/server.js
@@ -2,12 +2,23 @@ const { urlencoded } = require('express')
 const express = require('express')
 var route=require('./router/route')
 var app=express()
-
+ 
+ const path = require('path');
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 app.use(route)
  app.post('/signup',(req,res)=>{
     res.send(req.body.name)
  })
+
+ 
+ 
+ app.use(express.static(path.join(__dirname, 'build')));
+ 
+ app.get('/*', function (req, res) {
+   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+ });
+ 
+ 
 
 app.listen(process.env.PORT || 5000)
